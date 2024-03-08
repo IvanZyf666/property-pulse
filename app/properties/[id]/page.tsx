3049a2e0b8775301e349c30cf7f1713b2956c1c9
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-08 16:53:59 -0700

    add client hooks

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,9 +1,24 @@
+"use client";
 import React from "react";
+import { useRouter, useParams, useSearchParams } from "next/navigation";
 
 type Props = {};
 
 const PropertyPage = (props: Props) => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  // /properties/132
+  const { id } = useParams();
+  // /properties/132?name=iamtest
+  const searchParams = useSearchParams();
+  const name = searchParams.get("name");
+
+  return (
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home Id:{id} {name && `Name: ${name}`}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
