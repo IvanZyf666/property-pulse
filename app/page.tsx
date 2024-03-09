cef38218c092096931822cc6e4ac8d8e2f90f289
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-08 20:52:13 -0700

    add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 type Props = {};
 
-const page = (props: Props) => {
+const HomePage = (props: Props) => {
   return (
-    <div className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-3xl"> Welcome </h1>
-      <Link href="/properties"> Show Properties </Link>
+    // <div className="flex min-h-screen flex-col items-center p-24">
+    <div>
+      <Hero />
+      <InfoBoxes />
     </div>
   );
 };
 
-export default page;
+export default HomePage;
