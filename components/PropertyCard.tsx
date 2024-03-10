ff46006de2a35e17738509807f1205eda00407a0
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-09 17:31:33 -0700

    update card

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,20 +1,26 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { PropertyInterface } from "@/interfaces/PropertyInterface";
 
 type Props = {
-  property: any;
+  property: PropertyInterface;
 };
 
-const PropertyCard = (props: Props) => {
+const PropertyCard = ({ property }: Props) => {
   return (
     <div className="rounded-xl shadow-md relative">
-      <img
-        src="images/properties/a1.jpg"
+      <Image
+        src={`/images/properties/${property.images[0]}`}
         alt=""
+        height={0}
+        width={0}
+        sizes="100vw"
         className="w-full h-auto rounded-t-xl"
       />
       <div className="p-4">
         <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">Apartment</div>
+          <div className="text-gray-600"> {property.type} </div>
           <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
         </div>
         <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
