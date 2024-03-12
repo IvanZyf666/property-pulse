da0a3a822ae565c4c5c99a1d12cbb5ed73d25672
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-11 22:34:59 -0600

    add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import Link from "next/link";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 type Props = {};
 
 const HomePage = (props: Props) => {
   return (
-    // <div className="flex min-h-screen flex-col items-center p-24">
-    <div>
+    <>
       <Hero />
       <InfoBoxes />
-    </div>
+      <HomeProperties />
+    </>
   );
 };
 
