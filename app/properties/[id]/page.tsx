07af2be3de82186c5ac6780f622c4798f89d8f55
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-12 20:45:47 -0600

    add not found

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,8 +1,22 @@
 "use client";
 import React from "react";
-import { useRouter, useParams, useSearchParams } from "next/navigation";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  redirect,
+} from "next/navigation";
 
 type Props = {};
+
+const getPropertyInfo = (properties: any, id_string: any) => {
+  var id: number = +id_string; // convert to number
+  const property = properties.at(id);
+  if (!property) redirect("/not-found");
+  return property;
+};
 
 const PropertyPage = (props: Props) => {
   const router = useRouter();
@@ -12,11 +26,16 @@ const PropertyPage = (props: Props) => {
   const searchParams = useSearchParams();
   const name = searchParams.get("name");
 
+  const property = getPropertyInfo(properties, id);
+
   return (
     <div>
       <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
         Go Home Id:{id} {name && `Name: ${name}`}
       </button>
+      <div className="flex flex-wrap justify-center max-w-xl min-w-64 mx-40">
+        <PropertyCard property={property} />
+      </div>
     </div>
   );
 };
