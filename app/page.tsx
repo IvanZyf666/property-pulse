e9939e95f5fb449e1b4fd42f201f7c014d83d809
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-15 19:59:08 -0600

    add api route

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
-// import connectDB from "@/config/database";
 
 type Props = {};
 
-const HomePage = async (props: Props) => {
-  // await connectDB();
+const HomePage = (props: Props) => {
   return (
     <>
       <Hero />
