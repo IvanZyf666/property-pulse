23f8ed95fc183169d093835d0631c259fa298c41
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-15 22:15:38 -0600

    fix

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -37,8 +37,8 @@ const PropertyCard = ({ property }: Props) => {
       />
       <div className="p-4">
         <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600"> {property.type} </div>
-          <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
         <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
           ${getRateDisplay()}
