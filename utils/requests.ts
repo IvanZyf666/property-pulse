80d3c37f665743975aacbb416356ec5efd12675c
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-15 22:07:27 -0600

    add single query

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,6 +1,7 @@
 import propertiesOrigin from "@/properties.json";
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch all properties
 async function fetchProperties() {
   try {
     if (!apiDomain) {
@@ -19,4 +20,32 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch single property
+async function fetchProperty(id: string) {
+  try {
+    if (!apiDomain) {
+      return getDefaultPropertyInfo(propertiesOrigin, id);
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return getDefaultPropertyInfo(propertiesOrigin, id);
+  }
+}
+
+const getDefaultPropertyInfo = (properties: any, id_string: any) => {
+  var id: number = +id_string; // convert to number
+  if (typeof id !== "number" || !(id > 0 && id < 11)) {
+    return null;
+  }
+  const property = properties.at(id - 1);
+  return property;
+};
+
+export { fetchProperties, fetchProperty };
