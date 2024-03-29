6d3ec612e32ff394bedbe32dcec31aa8fd7e17f2
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-29 13:39:14 -0600

    update search results

--- app/properties/search-results/page.tsx
+++ app/properties/search-results/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
+import { PropertyInterface } from "@/interfaces/PropertyInterface";
 
 type Props = {};
 
@@ -34,7 +39,30 @@ const SearchResultsPage = (props: Props) => {
     fetchSearchResults();
   }, [location, propertyType]);
 
-  return <div>SearchResultsPage</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <Link
+          href="/properties"
+          className="flex items-center text-blue-500 hover:underline mb-2"
+        >
+          <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back to Properties
+        </Link>
+        <h1 className="text-2xl mb-4 font-bold">Search Results</h1>
+        {properties.length === 0 ? (
+          <p> No search results found </p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property: PropertyInterface) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default SearchResultsPage;
