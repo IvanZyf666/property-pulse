b97a5d7598b88b99dfd9851e2b7bbf8ce26345c2
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-28 20:17:11 -0600

    fix bug

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,16 @@
+"use client";
 import React from "react";
-import Link from "next/link";
 import { FaExclamationTriangle } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 const NotFoundPage = (props: Props) => {
+  const router = useRouter();
+  const handleBackToHome = () => {
+    router.replace("/");
+  };
+
   return (
     <section className="bg-blue-50 min-h-screen flex-grow">
       <div className="container m-auto max-w-2xl py-24">
@@ -14,15 +20,15 @@ const NotFoundPage = (props: Props) => {
           </div>
           <div className="text-center">
             <h1 className="text-3xl font-bold mt-4 mb-2">Page Not Found</h1>
-            <p className="text-gray-500 text-xl mb-10">
+            <p className="text-gray-500 text-xl mb-8">
               The page you are looking for does not exist.
             </p>
-            <Link
-              href="/"
-              className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded"
+            <button
+              className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-8 rounded"
+              onClick={() => handleBackToHome()}
             >
               Go Home
-            </Link>
+            </button>
           </div>
         </div>
       </div>
