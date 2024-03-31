81b8cfed882cc332e2b9a2d9051f3d2426b03236
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-30 22:38:14 -0600

    add redirect

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,18 +1,34 @@
+"use client";
 import React from "react";
 import PropertyAddForm from "@/components/PropertyAddForm";
+import { useUser } from "@clerk/clerk-react";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 const PropertyAddPage = (props: Props) => {
-  return (
-    <section className="bg-blue-50">
-      <div className="container m-auto max-w-2xl py-24">
-        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-          <PropertyAddForm />
+  const { isSignedIn, user, isLoaded } = useUser();
+  const router = useRouter();
+  if (!isLoaded) {
+    // Handle loading state however you like
+    return null;
+  }
+
+  if (isSignedIn) {
+    return (
+      <section className="bg-blue-50">
+        <div className="container m-auto max-w-2xl py-24">
+          <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+            <PropertyAddForm />
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
+
+  // not signed in, redirect to sign in page
+  router.push("/sign-in");
+  return null;
 };
 
 export default PropertyAddPage;
