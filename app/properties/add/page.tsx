1b601561153dc552bb3ed9911aeb480aa91da5a5
IvanZyf666 <yzhang24@ualberta.ca>
2024-03-30 22:00:21 -0600

    update add

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import PropertyAddForm from "@/components/PropertyAddForm";
 
 type Props = {};
 
 const PropertyAddPage = (props: Props) => {
   return (
-    <div>
-      PropertyAddPage
-      <div></div>
-    </div>
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
   );
 };
 
